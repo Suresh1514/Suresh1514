7aced77deaf2f7af45aec976baeed213f2be9916
Suresh1514 <156294059+Suresh1514@users.noreply.github.com>
2025-07-03 14:30:11 +0530

    Update Drug project.py

--- Drug project.py
+++ Drug project.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+!pip install matplotlib
 import seaborn as sns
 from wordcloud import WordCloud, STOPWORDS
 import re

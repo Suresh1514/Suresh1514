4932a91f24fd76b4b33c6a8f4dab691eab031e95
Suresh1514 <156294059+Suresh1514@users.noreply.github.com>
2025-07-04 08:40:19 +0530

    Update Drug project.py

--- Drug project.py
+++ Drug project.py
@@ -1,4 +1,11 @@
-
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from wordcloud import WordCloud, STOPWORDS
+import re
+from textblob import TextBlob  # For sentiment analysis
 
 # Set page configuration
 st.set_page_config(

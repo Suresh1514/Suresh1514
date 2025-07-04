1b368bd04cdcc137e81f25855c1cc1125213d0a0
Suresh1514 <156294059+Suresh1514@users.noreply.github.com>
2025-07-04 07:16:39 +0530

    Update Drug project.py

--- Drug project.py
+++ Drug project.py
@@ -7,11 +7,7 @@ from wordcloud import WordCloud, STOPWORDS
 import re
 from textblob import TextBlob  # For sentiment analysis
 
-   import matplotlib.pyplot as plt
-except ImportError:
-    import subprocess
-    subprocess.run(['pip', 'install', 'matplotlib'])
-    import matplotlib.pyplot as plt
+ 
 
 # Set page configuration
 st.set_page_config(
